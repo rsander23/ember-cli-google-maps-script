c0677bfc22312c96d1c48d06c186d816edb1566f
John Acosta <johnjaiverac@gmail.com>
2015-09-23 12:04:20 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 /* jshint node: true */
 'use strict';
 
+var url = '//maps.googleapis.com/maps/api/js?sensor=false';
+
 module.exports = {
-  name: 'ember-cli-google-maps-script'
+  name: 'ember-cli-google-maps-script',
+  contentFor: function(type, config) {
+    var env = config.environment;
+    var configuration = config['google-maps-script'];
+    var clientId = configuration.clientId;
+
+    if (type === 'head-footer' && env !== 'test') {
+      if (clientId) {
+        url += '&client=' + clientId;
+      }
+    }
+  }
 };
