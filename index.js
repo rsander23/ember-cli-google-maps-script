a469256832b0c781cc58d080513b28afed37362d
Marcelo Mira <hello@marcemira.com>
2016-06-14 14:52:40 +0200

    Updated options
    Added sensor and callback parameters

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 /* jshint node: true */
 'use strict';
 
-var url = '//maps.googleapis.com/maps/api/js?sensor=false';
+var url = '//maps.googleapis.com/maps/api/js?';
 
 module.exports = {
   name: 'ember-cli-google-maps-script',
   contentFor: function(type, config) {
     var env = config.environment;
     var configuration = config['google-maps-script'];
-    var clientId = configuration.clientId;
-    var key = configuration.key;
+    var sensor = configuration.sensor || 'false';
+    var clientId = configuration.clientId || null;
+    var key = configuration.key || null;
+    var callBack = configuration.callback || null;
+
+    url += 'sensor=' + sensor;
 
     if (type === 'head-footer' && env !== 'test') {
       if (clientId) {
@@ -18,6 +22,10 @@ module.exports = {
 
       if (key) {
         url += '&key=' + key;
+      }
+      
+      if (callBack) {
+        url += '&callback=' + callBack
       }
 
       return '<script src="' + url + '"></script>';
