8e398035fda5673f3b379c7b6b7fdfc7e0ea8c16
Marcelo Mira <hello@marcemira.com>
2016-06-14 15:32:45 +0200

    Fixed url duplications, testeable
    Fixed url param duplication, re-enabled test environment, fixed typos

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ var url = '//maps.googleapis.com/maps/api/js?';
 module.exports = {
   name: 'ember-cli-google-maps-script',
   contentFor: function(type, config) {
-    var env = config.environment;
     var configuration = config['google-maps-script'];
     var sensor = configuration.sensor || 'false';
     var clientId = configuration.clientId || null;
     var key = configuration.key || null;
     var callBack = configuration.callback || null;
 
-    url += 'sensor=' + sensor;
+    if (type === 'head-footer') {
+      
+      url += 'sensor=' + sensor;
 
-    if (type === 'head-footer' && env !== 'test') {
       if (clientId) {
         url += '&client=' + clientId;
       }
@@ -25,7 +25,7 @@ module.exports = {
       }
       
       if (callBack) {
-        url += '&callback=' + callBack
+        url += '&callback=' + callBack;
       }
 
       return '<script src="' + url + '"></script>';
